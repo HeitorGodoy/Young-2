0db9c78a983635369bfd87c2b5aa703d07082716
LeLeRD <136902806+HeitorGodoy@users.noreply.github.com>
2023-10-07 10:02:22 -0300

    Aula 8
    inicio JavaScript

---
+++ Aula-8/script.js
@@ -0,0 +1,28 @@
+// document.getElementById('titulo').innerHTML = 'Bom Dia'
+// document.write(5+5)
+// console.log('teste 123')
+// let resposta = window.prompt('Digite um número')
+// console.log(resposta)
+// document.write(resposta)
+
+// let r = window.prompt('Digite um Número')
+// let num = resposta %2
+
+// if (num == 0){
+//     document.write('É par')
+// }
+// else {
+//     document.write('É impar')
+// }
+
+// let n1 = parseInt(window.prompt('Digite um Número'))
+// let n2 = parseInt(window.prompt('Digite outro Número'))
+// resposta = (n1+n2)
+// document.write(resposta)
+
+let n1 = parseInt(window.prompt('Digite um número para sua tabuada'))
+for (let x =1; x<11; x++){
+    document.write(` ,${n1} X ${x} = ${n1*x}`)
+}
+
+document.write(resposta)
