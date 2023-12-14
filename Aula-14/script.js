0b7e3487ff5131021ec639d93081bf0977dde2fa
HeitorGodoy <136902806+HeitorGodoy@users.noreply.github.com>
2023-12-14 11:15:36 -0300

    pokedex finalizada

--- Aula-14/script.js
+++ Aula-14/script.js
@@ -1,17 +1,17 @@
-const NAME_POKE = Document.querySelector('.name_poke')
-const NUMBER_POKE = Document.querySelector('.num_poke')
-const IMG_POKE = Document.querySelector('.poke_img')
-const FORM = Document.querySelector('.forms')
-const INPUT = Document.querySelector('.input_search')
-const PREV_BTN = Document.querySelector('.pre_btn')
-const NEXT_BTN = Document.querySelector('.next_btn')
+const NAME_POKE = document.querySelector('.name_poke')
+const NUMBER_POKE = document.querySelector('.num_poke')
+const IMG_POKE = document.querySelector('.poke_img')
+const FORM = document.querySelector('.forms')
+const INPUT = document.querySelector('.input_search')
+const PREV_BTN = document.querySelector('.pre_btn')
+const NEXT_BTN = document.querySelector('.next_btn')
 
 let id_pokemon = 1
 
 const buscar_pokemon = async (pokemon) => {
-    resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu/${pokemon}`)
+    resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     if (resposta.status === 200) {
-        const info = resposta.json
+        const info = resposta.json()
         return info
     }
 }
@@ -21,6 +21,32 @@ const mostrar_pokemon = async (pokemon) => {
 
     if (json_pokemon) {
         IMG_POKE.src = json_pokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        IMG_POKE.style.display = 'block'
+        NAME_POKE.innerHTML = json_pokemon.name
+        NUMBER_POKE.innerHTML = json_pokemon.id
+        id_pokemon = json_pokemon.id
+        INPUT.value = ''
+    }
+    else {
+        IMG_POKE.style.display = 'none'
+        NAME_POKE.innerHTML = 'Not Founded'
+        NUMBER_POKE.innerHTML = ''
     }
 
 }
+FORM.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    let pokemon = INPUT.value.toLowerCase()
+    mostrar_pokemon(pokemon)
+})
+PREV_BTN.addEventListener('click', () => {
+    if (id_pokemon > 1) {
+        id_pokemon -= 1
+        mostrar_pokemon(id_pokemon)
+    }
+})
+NEXT_BTN.addEventListener('click', () => {
+    id_pokemon += 1
+    mostrar_pokemon(id_pokemon)
+})
+mostrar_pokemon(id_pokemon)
